3424b79170834cb59f6390280f417dad71136811
ramagarciapedrosa <rgarcia@makingsense.com>
2024-04-24 21:01:15 -0300

    updates + pp script

--- .config/qtile/scripts/storage.py
+++ .config/qtile/scripts/storage.py
@@ -4,10 +4,19 @@ import shutil
 
 
 def format_bytes(num_bytes):
+    """
+    Convert a number of bytes into a human-readable format using binary prefixes.
+
+    Args:
+        num_bytes (int): The number of bytes.
+
+    Returns:
+        str: The formatted string with the appropriate unit.
+    """
     power = 2**10
     n = 0
     power_labels = {0: '', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
-    while num_bytes > power:
+    while num_bytes >= power:
         num_bytes /= power
         n += 1
     return f"{round(num_bytes, 2)}{power_labels[n]}"
@@ -20,4 +29,4 @@ def diskspace(mode, media=False):
         'DiskUsage': f'{format_bytes(used)} / {format_bytes(total)}',
         'FreeSpace': f'{format_bytes(free)}'
     }
-    return data_disk[mode]
+    return data_disk.get(mode, "Invalid mode specified")
